13da0a6930712020dacad57cbe85987c180e9361
kelly r maass <kmaass77627@gmail.com>
2020-09-18 09:29:31 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -2,9 +2,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
 console.log(`Hello World`);
 
 
- let counter = document.getElementById('counter').value
+ let counter = document.getElementById('counter')
  for (let i = 0; i < 100; i++) {
-   counter = i
+   counter.value = i;
  }
 
 
